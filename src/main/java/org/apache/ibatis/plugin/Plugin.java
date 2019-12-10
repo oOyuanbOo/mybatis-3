cb3b39c2be65cc7bda119667ee6027b9dbe0a18e
oOyuanbOo <yuan3910@gmail.com>
2019-12-10 16:20:49 +0800

    work merge

--- src/main/java/org/apache/ibatis/plugin/Plugin.java
+++ src/main/java/org/apache/ibatis/plugin/Plugin.java
@@ -41,16 +41,11 @@ public class Plugin implements InvocationHandler {
   }
 
   public static Object wrap(Object target, Interceptor interceptor) {
-<<<<<<< HEAD
-    Map<Class<?>, Set<Method>> signatureMap = getSignatureMap(interceptor);
-    Class<?> type = target.getClass();
-=======
     // 获取要增强的方法
     Map<Class<?>, Set<Method>> signatureMap = getSignatureMap(interceptor);
     // 获取委托类
     Class<?> type = target.getClass();
     // 获取所有的接口，这里为什么要自定义获取接口的方法，哦，这个signatureMethod是不是mapper注解上的方法，select之类的
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
     Class<?>[] interfaces = getAllInterfaces(type, signatureMap);
     if (interfaces.length > 0) {
       return Proxy.newProxyInstance(
@@ -65,9 +60,7 @@ public class Plugin implements InvocationHandler {
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
     try {
       Set<Method> methods = signatureMap.get(method.getDeclaringClass());
-<<<<<<< HEAD
-      if (methods != null && methods.contains(method)) {
-=======
+
       // 如果@signature注解的method指明要拦截某个方法，那么久调用intercept方法，否则直接执行委托类的方法
       // 所以intercept才是真的牛批
       // 它的参数invocation相当于委托类的一层包装，在proceed方法里面调用委托类的invoke，这和我之前实现的拦截器的区别
@@ -79,7 +72,7 @@ public class Plugin implements InvocationHandler {
       if (methods != null && methods.contains(method)) {
         // 这里还是调用的接口，具体要看实现
         // 关于intercept方法的这个参数Invocation，要理解下
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
         return interceptor.intercept(new Invocation(target, method, args));
       }
       return method.invoke(target, args);
@@ -88,15 +81,14 @@ public class Plugin implements InvocationHandler {
     }
   }
 
-<<<<<<< HEAD
-=======
+
   /**
    * 这里看着像是从注解获取一些参数
    * 找到的method应该就是增强的方法吧   待确认
    * @param interceptor
    * @return
    */
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
   private static Map<Class<?>, Set<Method>> getSignatureMap(Interceptor interceptor) {
     Intercepts interceptsAnnotation = interceptor.getClass().getAnnotation(Intercepts.class);
     // issue #251
