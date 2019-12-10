fd7c72c0ea87703021eed0af762d97e0a4146003
oOyuanbOo <yuan3910@gmail.com>
2019-12-10 15:53:24 +0800

    work merge

--- src/main/java/org/apache/ibatis/reflection/MetaClass.java
+++ src/main/java/org/apache/ibatis/reflection/MetaClass.java
@@ -19,13 +19,12 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-=======
 import java.util.Collection;
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
 
 import org.apache.ibatis.reflection.invoker.GetFieldInvoker;
 import org.apache.ibatis.reflection.invoker.Invoker;
@@ -34,7 +33,7 @@ import org.apache.ibatis.reflection.property.PropertyTokenizer;
 
 /**
  * @author Clinton Begin
-<<<<<<< HEAD
+
  * 前面看的都是铺垫，这里该是那些工具类来个华山论剑的地方了
  * 类的元数据，对类的增强，与MetaObject相对应
  */
@@ -43,11 +42,6 @@ public class MetaClass {
   /**
    * 两个工具类摆在这里
    */
-=======
- */
-public class MetaClass {
-
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
   private final ReflectorFactory reflectorFactory;
   private final Reflector reflector;
 
@@ -56,54 +50,50 @@ public class MetaClass {
     this.reflector = reflectorFactory.findForClass(type);
   }
 
-<<<<<<< HEAD
+
   /**
    * 给这个类整个工具类MetaClass，相当于包装了一层外置骨骼
    * @param type
    * @param reflectorFactory
    * @return
    */
-=======
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
   public static MetaClass forClass(Class<?> type, ReflectorFactory reflectorFactory) {
     return new MetaClass(type, reflectorFactory);
   }
 
-<<<<<<< HEAD
+
   /**
    * 属性也可以单独包装
    * @param name
    * @return
    */
-=======
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
   public MetaClass metaClassForProperty(String name) {
     Class<?> propType = reflector.getGetterType(name);
     return MetaClass.forClass(propType, reflectorFactory);
   }
 
-<<<<<<< HEAD
+
   /**
    * 不知道有什么作用，先不管，反正就是从name中获取属性
    * @param name
    * @return
    */
-=======
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
   public String findProperty(String name) {
     StringBuilder prop = buildProperty(name, new StringBuilder());
     return prop.length() > 0 ? prop.toString() : null;
   }
 
-<<<<<<< HEAD
+
   /**
    * 这个是从驼峰式的命名字符串中获取属性
    * @param name
    * @param useCamelCaseMapping
    * @return
    */
-=======
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
   public String findProperty(String name, boolean useCamelCaseMapping) {
     if (useCamelCaseMapping) {
       name = name.replace("_", "");
@@ -111,33 +101,30 @@ public class MetaClass {
     return findProperty(name);
   }
 
-<<<<<<< HEAD
+
   /**
    * 获取所有的getter方法
    * @return
    */
-=======
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
   public String[] getGetterNames() {
     return reflector.getGetablePropertyNames();
   }
 
-<<<<<<< HEAD
+
   /**
    * 获取所有的setter方法
    * @return
    */
-=======
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
   public String[] getSetterNames() {
     return reflector.getSetablePropertyNames();
   }
 
   public Class<?> getSetterType(String name) {
-<<<<<<< HEAD
+
     // 每个属性都要加工成这个吊样，芋道上说是进行分词
-=======
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
     PropertyTokenizer prop = new PropertyTokenizer(name);
     if (prop.hasNext()) {
       MetaClass metaProp = metaClassForProperty(prop.getName());
@@ -147,7 +134,7 @@ public class MetaClass {
     }
   }
 
-<<<<<<< HEAD
+
   /**
    * 获取getter的返回类型
    * @param name
@@ -158,11 +145,6 @@ public class MetaClass {
     PropertyTokenizer prop = new PropertyTokenizer(name);
     if (prop.hasNext()) {
       // 获取属性
-=======
-  public Class<?> getGetterType(String name) {
-    PropertyTokenizer prop = new PropertyTokenizer(name);
-    if (prop.hasNext()) {
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
       MetaClass metaProp = metaClassForProperty(prop);
       return metaProp.getGetterType(prop.getChildren());
     }
@@ -176,15 +158,8 @@ public class MetaClass {
   }
 
   private Class<?> getGetterType(PropertyTokenizer prop) {
-<<<<<<< HEAD
-    Class<?> type = reflector.getGetterType(prop.getName());
-    if (prop.getIndex() != null && Collection.class.isAssignableFrom(type)) {
-      Type returnType = getGenericGetterType(prop.getName());
-      if (returnType instanceof ParameterizedType) {
-        Type[] actualTypeArguments = ((ParameterizedType) returnType).getActualTypeArguments();
         // 返回类型的泛型参数只有一个？ 比如List<String>这种，而不是Map<String, Object>
         // 这个你是如何得出的呢，找找public方法，看看测试怎么处理
-=======
     // 获取属性get方法的返回类型，map真的是个很有用的数据结构，相当于一个缓存，放在类里面，随用随取
     Class<?> type = reflector.getGetterType(prop.getName());
     // 如果下标不为空，并且是集合类的儿子，那么能断定这是个集合类的属性
@@ -198,7 +173,7 @@ public class MetaClass {
         // <?>这个在五大类型中是类型变量(TypeVariable)
         Type[] actualTypeArguments = ((ParameterizedType) returnType).getActualTypeArguments();
         // TODO：说实话，不打断点，我都不知道actualTypeArguments是个啥
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
         if (actualTypeArguments != null && actualTypeArguments.length == 1) {
           returnType = actualTypeArguments[0];
           if (returnType instanceof Class) {
@@ -214,15 +189,6 @@ public class MetaClass {
 
   private Type getGenericGetterType(String propertyName) {
     try {
-<<<<<<< HEAD
-      Invoker invoker = reflector.getGetInvoker(propertyName);
-      if (invoker instanceof MethodInvoker) {
-        Field _method = MethodInvoker.class.getDeclaredField("method");
-        _method.setAccessible(true);
-        Method method = (Method) _method.get(invoker);
-        return TypeParameterResolver.resolveReturnType(method, reflector.getType());
-      } else if (invoker instanceof GetFieldInvoker) {
-=======
       // 获取泛型的时候用到了getInvoker，里面貌似就一个invoke方法，以及一个getType
       Invoker invoker = reflector.getGetInvoker(propertyName);
       // 如果是MethodInvoker的实现
@@ -241,7 +207,7 @@ public class MetaClass {
       } else if (invoker instanceof GetFieldInvoker) {
         // 区别是上面获取的是属性，哦~~   难道是那些不需要get的方法，比如直接在属性声明的时候初始化
         // 应该就是！   因为这个Invoker里面有只field成员变量
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
         Field _field = GetFieldInvoker.class.getDeclaredField("field");
         _field.setAccessible(true);
         Field field = (Field) _field.get(invoker);
@@ -253,12 +219,6 @@ public class MetaClass {
   }
 
   public boolean hasSetter(String name) {
-<<<<<<< HEAD
-    PropertyTokenizer prop = new PropertyTokenizer(name);
-    if (prop.hasNext()) {
-      if (reflector.hasSetter(prop.getName())) {
-        MetaClass metaProp = metaClassForProperty(prop.getName());
-=======
     // 拆词都是第一步
     PropertyTokenizer prop = new PropertyTokenizer(name);
     if (prop.hasNext()) {
@@ -267,7 +227,7 @@ public class MetaClass {
         // 把属性包装成MetaClass
         MetaClass metaProp = metaClassForProperty(prop.getName());
         // 然后递归，方法优雅的地方在于prop是封装好的遍历器，所以你可以不停的调自己，直到不满足条件执行其他逻辑
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
         return metaProp.hasSetter(prop.getChildren());
       } else {
         return false;
@@ -291,10 +251,9 @@ public class MetaClass {
     }
   }
 
-<<<<<<< HEAD
-=======
+
   // 都是调用reflector的方法，可以这么说MetaClass就是Reflector的又一层外骨骼
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
+
   public Invoker getGetInvoker(String name) {
     return reflector.getGetInvoker(name);
   }
@@ -303,11 +262,6 @@ public class MetaClass {
     return reflector.getSetInvoker(name);
   }
 
-<<<<<<< HEAD
-  private StringBuilder buildProperty(String name, StringBuilder builder) {
-    // 先包装一层，可以一级一级遍历
-    PropertyTokenizer prop = new PropertyTokenizer(name);
-=======
   /**
    * 这是将属性名用.连起来
    * name 可能是一串字符  prop1.prop11.prop11
@@ -317,9 +271,9 @@ public class MetaClass {
    * @return
    */
   private StringBuilder buildProperty(String name, StringBuilder builder) {
-    PropertyTokenizer prop = new PropertyTokenizer(name);
+      // 先包装一层，可以一级一级遍历
+      PropertyTokenizer prop = new PropertyTokenizer(name);
     // 又是递归，
->>>>>>> 5301c684afb0817920e573143b83a7605127b2e0
     if (prop.hasNext()) {
       String propertyName = reflector.findPropertyName(prop.getName());
       if (propertyName != null) {
